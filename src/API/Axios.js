d137b5d35a28030f53fe45db6569aef76d9f7b7f
tranvandoan <tranlove24@gmail.com>
2023-02-27 16:52:12 +0700

    upload api

--- src/API/Axios.js
+++ src/API/Axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const token = localStorage.getItem("token");
 export const axiosClient = axios.create({
   // baseURL: "http://localhost:9100/api",
-  baseURL: "https://back-end-order-app.vercel.app/api",
+  baseURL: "https://back-end-order.vercel.app/api",
   headers: {
     "Content-Type": "application/json",
     Authorization: `Bearer ${token?.slice(1, -1)}`,
